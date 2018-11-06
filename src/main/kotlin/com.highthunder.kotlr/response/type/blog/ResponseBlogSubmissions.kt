0dfcb11ca6589f9c3152b4bcf6365ed728663b9c
Eric Hochendoner <eric.hochendoner@gmail.com>
2018-11-06 01:48:27 -0500

    Add stubs of all request types

---
+++ src/main/kotlin/com.highthunder.kotlr/response/type/blog/ResponseBlogSubmissions.kt
@@ -0,0 +1,12 @@
+package com.highthunder.kotlr.response.type.blog
+
+/**
+ * ${FILE} - TODO: Documentation
+ *
+ * @author highthunder
+ * @since 11/4/18
+ * @version 1.0.0
+ */
+interface ResponseBlogSubmissions {
+
+}
---
+++ src/main/kotlin/com.highthunder.kotlr/response/type/user/ResponseUserLikes.kt
@@ -0,0 +1,11 @@
+package com.highthunder.kotlr.response.type.user
+
+/**
+ * ${FILE} - TODO: Documentation
+ *
+ * @author highthunder
+ * @since 11/4/18
+ * @version 1.0.0
+ */
+interface ResponseUserLikes {
+}
