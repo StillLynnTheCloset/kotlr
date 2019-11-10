13ce7fa17372c7505ed42a0450482d43d67cb88b
Eric Hochendoner <eric.hochendoner@gmail.com>
2019-11-10 12:18:58 -0500

    Update version in properties file as well

--- src/main/kotlin/com.highthunder.kotlr/Tools.kt
+++ src/main/kotlin/com.highthunder.kotlr/Tools.kt
@@ -4,9 +4,14 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 
-internal inline fun <reified T> Moshi.adapter(): JsonAdapter<T> = adapter(T::class.java)
-internal inline fun <reified T> Moshi.listAdapter(): JsonAdapter<List<T>> = adapter(Types.newParameterizedType(List::class.java, T::class.java))
-internal inline fun <reified K, reified V> Moshi.mapAdapter(): JsonAdapter<Map<K, V>> = adapter(Types.newParameterizedType(List::class.java, K::class.java, V::class.java))
+internal inline fun <reified T> Moshi.adapter(): JsonAdapter<T> =
+    adapter(T::class.java)
+
+internal inline fun <reified T> Moshi.listAdapter(): JsonAdapter<List<T>> =
+    adapter(Types.newParameterizedType(List::class.java, T::class.java))
+
+internal inline fun <reified K, reified V> Moshi.mapAdapter(): JsonAdapter<Map<K, V>> =
+    adapter(Types.newParameterizedType(List::class.java, K::class.java, V::class.java))
 
 internal inline fun justTry(block: () -> Unit) {
     try {
