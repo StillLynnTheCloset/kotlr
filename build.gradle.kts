0771ce8aca5434fb0d90f2ff242b33553f8a4e3d
Eric Hochendoner <eric.hochendoner@gmail.com>
2020-09-06 15:52:43 -0400

    Update plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ buildscript {
 plugins {
     kotlin("jvm") version "1.4.0"
     kotlin("kapt") version "1.4.0"
-    id("com.github.ben-manes.versions") version "0.29.0"
+    id("com.github.ben-manes.versions") version "0.30.0"
     id("de.fayard.buildSrcVersions") version "0.7.0"
     // id("de.fayard.refreshVersions") version "0.7.0" // Soon this will be the future!
-    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
+    id("org.jlleitschuh.gradle.ktlint") version "9.4.0"
     id("idea")
 }
 
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -22,9 +22,9 @@ object Versions {
 
     const val com_squareup_moshi: String = "1.10.0"
 
-    const val com_github_ben_manes_versions_gradle_plugin: String = "0.29.0"
+    const val com_github_ben_manes_versions_gradle_plugin: String = "0.30.0"
 
-    const val org_jlleitschuh_gradle_ktlint_gradle_plugin: String = "9.3.0"
+    const val org_jlleitschuh_gradle_ktlint_gradle_plugin: String = "9.4.0"
 
     const val de_fayard_buildsrcversions_gradle_plugin: String = "0.7.0"
 
