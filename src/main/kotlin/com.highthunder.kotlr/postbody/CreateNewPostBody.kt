f102ff0a42d1fa69799662064f5060b539334390
Lynn Hochendoner <lynn.hochendoner@gmail.com>
2021-06-05 01:07:25 -0400

    Add support for date, isPrivate, and slug properties when creating/reblogging a post

--- src/main/kotlin/com.highthunder.kotlr/postbody/CreateNewPostBody.kt
+++ src/main/kotlin/com.highthunder.kotlr/postbody/CreateNewPostBody.kt
@@ -12,10 +12,13 @@ import com.squareup.moshi.JsonClass
  * @param layout An array of NPF layout objects to be used to lay out the post content.
  * @param state The initial state of the new post, such as "published" or "queued".
  * @param publishOn The exact date and time (ISO 8601 format) to publish the post, if desired. This parameter will be ignored unless the state parameter is "queue".
+ * @param date The exact date and time (ISO 8601 format) in the past to _backdate_ the post, if desired. This backdating does not apply to when the post shows up in the Dashboard.
  * @param tags A list of tags to associate with the post. Note: Any commas included in these strings will be interpreted by Tumblr as a delimiter.
  * @param sourceUrl A source attribution for the post content.
- * @param sendToFacebook Whether or not to share this via any connected Twitter account on post publish. Defaults to the blog's global setting.
- * @param sendToTwitter Whether or not to share this via any connected Facebook account on post publish. Defaults to the blog's global setting.
+ * @param sendToFacebook Whether or not to share this via any connected Facebook account on post publish. Defaults to the blog's global setting.
+ * @param sendToTwitter Whether or not to share this via any connected Twitter account on post publish. Defaults to the blog's global setting.
+ * @param isPrivate Whether this should be a private answer, if this is an answer.
+ * @param slug A custom URL slug to use in the post's permalink URL.
  *
  * @author highthunder
  * @since 2019-12-01
@@ -27,6 +30,7 @@ public data class CreateNewPostBody constructor(
     val state: Post.State? = null,
     @Json(name = "publish_on")
     val publishOn: String? = null,
+    val date: String? = null,
     @CommaSeparatedString
     val tags: List<String>? = null,
     @Json(name = "source_url")
@@ -35,6 +39,9 @@ public data class CreateNewPostBody constructor(
     val sendToTwitter: Boolean? = null,
     @Json(name = "send_to_facebook")
     val sendToFacebook: Boolean? = null,
+    @Json(name = "is_private")
+    val isPrivate: Boolean? = null,
+    val slug: Boolean? = null,
 ) {
     init {
         tags?.forEach { tag ->
--- src/main/kotlin/com.highthunder.kotlr/postbody/ReblogPostBody.kt
+++ src/main/kotlin/com.highthunder.kotlr/postbody/ReblogPostBody.kt
@@ -12,10 +12,13 @@ import com.squareup.moshi.JsonClass
  * @param layout An array of NPF layout objects to be used to lay out the post content.
  * @param state The initial state of the new post, such as "published" or "queued".
  * @param publishOn The exact date and time (ISO 8601 format) to publish the post, if desired. This parameter will be ignored unless the state parameter is "queue".
+ * @param date The exact date and time (ISO 8601 format) in the past to _backdate_ the post, if desired. This backdating does not apply to when the post shows up in the Dashboard.
  * @param tags A list of tags to associate with the post. Note: Any commas included in these strings will be interpreted by Tumblr as a delimiter.
  * @param sourceUrl A source attribution for the post content.
- * @param sendToFacebook Whether or not to share this via any connected Twitter account on post publish. Defaults to the blog's global setting.
- * @param sendToTwitter Whether or not to share this via any connected Facebook account on post publish. Defaults to the blog's global setting.
+ * @param sendToFacebook Whether or not to share this via any connected Facebook account on post publish. Defaults to the blog's global setting.
+ * @param sendToTwitter Whether or not to share this via any connected Twitter account on post publish. Defaults to the blog's global setting.
+ * @param isPrivate Whether this should be a private answer, if this is an answer.
+ * @param slug A custom URL slug to use in the post's permalink URL.
  * @param parentTumblelogUuid The unique public identifier of the Tumblelog that's being reblogged from.
  * @param parentPostId The unique public post ID being reblogged.
  * @param reblogKey The unique per-post hash validating that this is a genuine reblog action.
@@ -31,6 +34,7 @@ public data class ReblogPostBody constructor(
     val state: Post.State? = null,
     @Json(name = "publish_on")
     val publishOn: String? = null,
+    val date: String? = null,
     @CommaSeparatedString
     val tags: List<String>? = null,
     @Json(name = "source_url")
@@ -39,6 +43,9 @@ public data class ReblogPostBody constructor(
     val sendToTwitter: Boolean? = null,
     @Json(name = "send_to_facebook")
     val sendToFacebook: Boolean? = null,
+    @Json(name = "is_private")
+    val isPrivate: Boolean? = null,
+    val slug: Boolean? = null,
     @Json(name = "parent_tumblelog_uuid")
     val parentTumblelogUuid: String,
     @Json(name = "parent_post_id")
