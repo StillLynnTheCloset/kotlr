d815317d5c19dbd2503275563d3fd650d2c6e3be
Lynn Hochendoner <lynn.hochendoner@gmail.com>
2022-05-30 15:52:40 -0400

    Update Gradle versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
-    id("de.fayard.refreshVersions") version "0.10.0"
+    id("de.fayard.refreshVersions") version "0.40.1"
 }
 
 rootProject.name = "kotlr"
 
 include(":lib", ":demo")
+
+
+refreshVersions {
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
